940fab87ab3fc79d62707ee9e558f7920dd8bffb
Pedro Bittencourt <pedromonteirobittencourt@gmail.com>
2024-03-18 16:09:50 -0300

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container text-center">
+        <h1 className="mt-5">Tic Tac Toe</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
